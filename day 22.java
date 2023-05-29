b87972b921c5c5cde109ee714c3840ce43381bc5
Vaibhav Shrivastava <108504165+montoo2003@users.noreply.github.com>
2023-05-29 06:57:24 +0530

    day 22.java

---
+++ day 22.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/first-missing-positive/submissions/959279434/
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        
+        int ans[] = new int[nums.length + 1];
+        for(int i = 0; i < nums.length; i++){
+            int val = nums[i];
+            if(val > 0 && val <= nums.length) {
+                ans[val] = 1;
+            }
+        }
+        for(int i = 1; i < ans.length; i++){
+            if(ans[i] == 0){
+                return i;
+            }
+        }
+    return ans.length;
+    }
+}
