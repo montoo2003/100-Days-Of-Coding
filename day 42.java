27d3c076d2a159ae5026fadc7c088214544d754d
Vaibhav Shrivastava <108504165+montoo2003@users.noreply.github.com>
2023-06-16 06:24:14 +0530

    day 42.java

---
+++ day 42.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/longest-valid-parentheses/submissions/972252851/
+class Solution {
+    public int longestValidParentheses(String s) {
+        int ans = 0,left = 0,right = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                left++;
+            }else{
+                right++;
+            }if(left == right){
+                ans = Math.max(ans, left * 2);
+            }else if(right > left){
+                left = 0; right = 0;
+            }
+        }
+        left = 0; right = 0;
+        for(int i = s.length() - 1; i >= 0 ; i--){
+            if(s.charAt(i) == '('){
+                left++;
+            }else{
+                right++;
+            }if(left == right){
+                ans = Math.max(ans, right * 2);
+            }else if(right < left){
+                left = 0; right = 0;
+            }
+        }
+        return ans;
+    }
+}
