b6c08ca6a6c9fadb77dbee9206b12f2a3cdf46fc
Vaibhav Shrivastava <108504165+montoo2003@users.noreply.github.com>
2023-06-30 07:24:55 +0530

    day 57.java

---
+++ day 57.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode temp=head;
+        for(int i=0;i<k;i++){
+            if(temp==null){
+                return head;
+            }
+            temp=temp.next;
+        }
+        ListNode sp=reverseKGroup(temp,k);
+        ListNode prev=null;
+        ListNode curr=head;
+        for(int i=0;i<k;i++){
+            ListNode after =curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=after;
+        }
+        head.next=sp;
+        return prev;
+    }
+}
