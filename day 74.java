37bf4ffb65cffabde6b2fa411fe19ceb7dc903a7
Vaibhav Shrivastava <108504165+montoo2003@users.noreply.github.com>
2023-07-18 19:37:41 +0530

    day 74.java

---
+++ day 74.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/remove-linked-list-elements/submissions/
+
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        
+        if(head == null) return null;
+        while(head!=null && head.val == val) head = head.next;
+        ListNode temp = head;
+        while(temp!=null && temp.next !=null) {
+            if(temp.next.val == val) {
+                temp.next = temp.next.next;
+            }else{
+                temp = temp.next;
+            }
+        }
+        return head;
+    }
+}
